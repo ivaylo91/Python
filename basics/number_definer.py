db6bb4f8ffecabb9661cde3abae48ff937b48b20
ivaylo91 <ipenev91@gmail.com>
2024-09-25 11:58:34 +0300

    number_definer and word reverse tasks

---
+++ basics/number_definer.py
@@ -0,0 +1,9 @@
+number = float(input())
+
+if number == 0:
+    print("zero")
+elif number > 0:
+    if number < 1:
+        print("small positive")
+    elif number > 1000000:
+        print("large positive")
